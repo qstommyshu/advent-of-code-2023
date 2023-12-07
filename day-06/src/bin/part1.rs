d1f1bf55e0cc9c3b7bd51924fe4c484691561332
qstommyshu <qstommyshu@gmail.com>
2023-12-07 08:33:24 -0500

    add day2 part 1

---
+++ day-06/src/bin/part1.rs
@@ -0,0 +1,50 @@
+use std::iter::zip;
+
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+struct Race {
+    time: u32,
+    dist: u32,
+}
+
+fn part1(input: &str) -> String {
+    let (time, dist) = input.split_once("\n").unwrap();
+    let time: Vec<u32> = time
+        .split_whitespace()
+        .filter_map(|num| { num.parse::<u32>().ok() })
+        .collect();
+    let dist: Vec<u32> = dist
+        .split_whitespace()
+        .filter_map(|num| { num.parse::<u32>().ok() })
+        .collect();
+    let races = zip(time, dist).map(|(time, dist)| Race { time, dist });
+    races
+        .map(|race| {
+            // TODO: for loop map interesting
+            (0..=race.time)
+                .map(|elapsed| {
+                    let speed = elapsed;
+                    speed * (race.time - elapsed)
+                })
+                .filter(|&dist| dist > race.dist)
+                .count()
+        })
+        .product::<usize>()
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "Time:      7  15   30
+        Distance:  9  40  200";
+        assert_eq!("288".to_string(), part1(input));
+    }
+}
